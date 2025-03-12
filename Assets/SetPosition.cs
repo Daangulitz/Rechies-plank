25e2889bd7c88bb15c8b30c9890d51510da8dc84
Daangulitz <98341108+Daangulitz@users.noreply.github.com>
2025-03-12 14:11:00 +0100

    reset position working

--- Assets/SetPosition.cs
+++ Assets/SetPosition.cs
@@ -1,20 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.XR.CoreUtils;
 
 public class setPosition : MonoBehaviour
 {
+    public InputActionProperty recenterButton;
+    
+    public Transform target;
 
-
-
-    void Start()
+    private void Start()
     {
-        
+        recenter();
     }
 
     void Update()
     {
-        
+        if (recenterButton.action.WasPressedThisFrame())
+        {
+            recenter();
+        }
+    }
+
+    public void recenter()
+    {
+        XROrigin xrorigin = GetComponent<XROrigin>();
+        xrorigin.MoveCameraToWorldLocation(target.position);
+        xrorigin.MatchOriginUpOriginForward(target.up,target.forward);
     }
 }
